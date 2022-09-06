4be5c98f00eaf8bbf23e2955f5bab7aa300faf8e
zzz <92370369+zaki240@users.noreply.github.com>
2022-09-06 21:18:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,98 @@
 const calculator = {
-  displayNumber: '0',
-  operator: null,
-  firstNumber: null,
-  waitingForSecondNumber: false
-}
-
-function updateDisplay() {
-  document.querySelector("#displayNumber").innerText = calculator.displayNumber;
-}
-
-function clearCalculator() {
-  calculator.displayNumber = '0';
-  calculator.operator = null;
-  calculator.firstNumber = null;
-  calculator.waitingForSecondNumber = false;
-}
-
-function inputDigit(digit) {
-  if (calculator.displayNumber === '0') {
-    calculator.displayNumber = digit;
-  } else {
-    calculator.displayNumber += digit;
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false
   }
-}
-
-const buttons = document.querySelectorAll(".button");
-// console.log(buttons)
-for (let button of buttons){
-
-  button.addEventListener("click", function(event){
-
-    const target = event.target;
-
-    if (target.classList.contains('clear')) {
-      clearCalculator();
-      updateDisplay();
-      return;
-    }
-
-    if (target.classList.contains('operator')) {
-      handleOperator(target.innerText);
-      return;
-    }
-
-    if (target.classList.contains('equals')) {
-      performCalculation();
-      updateDisplay();
-      return;
-    }
-
-    inputDigit(target.innerText);
-    updateDisplay();
-  })
-
-}
-
-function inverseNumber() {
-  if (calculator.displayNumber === '0') {
-    return;
+  
+  function updateDisplay() {
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
   }
-  calculator.displayNumber = calculator.displayNumber * -1;
-}
-
-function handleOperator(operator) {
-  if (!calculator.waitingForSecondNumber) {
-    calculator.operator = operator;
-    calculator.waitingForSecondNumber = true;
-    calculator.firstNumber = calculator.displayNumber;
-
+  
+  function clearCalculator() {
     calculator.displayNumber = '0';
-  } else {
-    alert('Operator sudah ditetapkan');
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
   }
-}
-
-function performCalculation() {
-  if (calculator.firstNumber == null || calculator.operator == null) {
-    alert("Anda belum menetapkan operator");
-    return;
+  
+  function inputDigit(digit) {
+    if (calculator.displayNumber === '0') {
+      calculator.displayNumber = digit;
+    } else {
+      calculator.displayNumber += digit;
+    }
+  }
+  
+  const buttons = document.querySelectorAll(".button");
+  // console.log(buttons)
+  for (let button of buttons){
+  
+    button.addEventListener("click", function(event){
+  
+      const target = event.target;
+  
+      if (target.classList.contains('clear')) {
+        clearCalculator();
+        updateDisplay();
+        return;
+      }
+  
+      if (target.classList.contains('operator')) {
+        handleOperator(target.innerText);
+        return;
+      }
+  
+      if (target.classList.contains('equals')) {
+        performCalculation();
+        updateDisplay();
+        return;
+      }
+  
+      inputDigit(target.innerText);
+      updateDisplay();
+    })
+  
+  }
+  
+  function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+      return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+  }
+  
+  function handleOperator(operator) {
+    if (!calculator.waitingForSecondNumber) {
+      calculator.operator = operator;
+      calculator.waitingForSecondNumber = true;
+      calculator.firstNumber = calculator.displayNumber;
+  
+      calculator.displayNumber = '0';
+    } else {
+      alert('Operator sudah ditetapkan');
+    }
+  }
+  
+  function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+      alert("Anda belum menetapkan operator");
+      return;
+    }
+  
+    let result = 0;
+    if (calculator.operator === "+") {
+      result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else if (calculator.operator === "-") {
+      result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    } else if (calculator.operator === "/") {
+      result = parseInt(calculator.firstNumber) / parseInt(calculator.displayNumber);
+    } else if (calculator.operator === "x") {
+      result = parseInt(calculator.firstNumber) * parseInt(calculator.displayNumber);
+    }
+  
+    calculator.displayNumber = result;
   }
 
-  let result = 0;
-  if (calculator.operator === "+") {
-    result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
-  }
-
-  calculator.displayNumber = result;
-}
-calculator.operator = null
+calculator.operator = null;
+calculator.waitingForSecondNumber = false;
